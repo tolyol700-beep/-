b83f23e499b4fa1ed142ae04ffaaec4b51b6f642
tolyol700-beep <tolyol700@gmail.com>
2025-10-09 13:27:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,25 +4,28 @@ import os
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "your_bot_token_here")
 CHANNEL_ID = "@autoinsaider_1"
 
-# Проверенные источники (только те, которые точно работают)
+# Обновленные проверенные источники
 SOURCES = {
     "insurance": [
-        "https://www.banki.ru/xml/news.rss?type=insurance",
-        "https://www.insur-info.ru/rss/",
+        "https://www.vedomosti.ru/rss/rubric/insurance",  # Ведомости - страхование
+        "https://www.kommersant.ru/RSS/section-insurance.xml",  # Коммерсант - страхование
+        "https://www.rbc.ru/rbcnews.xml",  # РБК (есть страхование)
     ],
     "laws": [
-        "https://www.garant.ru/rss/auto/",
-        "https://www.gibdd.ru/rss/",
+        "https://www.vedomosti.ru/rss/rubric/law",  # Ведомости - право
+        "https://www.kommersant.ru/RSS/section-law.xml",  # Коммерсант - право
+        "https://rg.ru/rss/index.xml",  # Российская газета
     ],
     "humor": [
-        "https://www.drom.ru/rss/",
-        "https://fishki.net/rss/tag/avto/",
+        "https://fishki.net/rss/tag/avto/",  # Фишки - авто
+        "https://www.anekdot.ru/rss/export_j.xml",  # Анекдоты
+        "https://www.drom.ru/rss/",  # Drom
     ]
 }
 
 # Ключевые слова для фильтрации
 KEYWORDS = {
-    "insurance": ["ОСАГО", "страхование", "КАСКО", "полис", "страховая", "КБМ"],
-    "laws": ["ПДД", "закон", "ГИБДД", "штраф", "правила", "изменения", "автоправо"],
-    "humor": ["юмор", "прикол", "мем", "шутка", "смешно", "забавно"]
+    "insurance": ["ОСАГО", "страхование", "КАСКО", "полис", "страховая", "КБМ", "автострахование"],
+    "laws": ["ПДД", "закон", "ГИБДД", "штраф", "правила", "изменения", "автоправо", "водитель", "автомобиль"],
+    "humor": ["юмор", "прикол", "мем", "шутка", "смешно", "забавно", "анекдот"]
 }
