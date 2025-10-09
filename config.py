93a2196ae78ebf330602fa6b7989724baa531a28
tolyol700-beep <tolyol700@gmail.com>
2025-10-09 13:14:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,16 +7,16 @@ CHANNEL_ID = "@autoinsaider_1"
 # Проверенные источники (только те, которые точно работают)
 SOURCES = {
     "insurance": [
-        "https://www.banki.ru/news/insurance/rss/",
-        "https://www.akm.ru/rss/insurance/",
+        "https://www.banki.ru/xml/news.rss?type=insurance",
+        "https://www.insur-info.ru/rss/",
     ],
     "laws": [
         "https://www.garant.ru/rss/auto/",
-        "https://www.gibdd.ru/rss/news/",
+        "https://www.gibdd.ru/rss/",
     ],
     "humor": [
         "https://www.drom.ru/rss/",
-        "https://fishki.net/tag/avto/rss/",
+        "https://fishki.net/rss/tag/avto/",
     ]
 }
 
